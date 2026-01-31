d85e4ceebde883c8906ad411510a40f4dd8d3572
Vadim <snuux@yandex.ru>
2026-02-01 00:27:00 +0300

    Added WalletService and PlayerData

---
+++ Assets/_Project/Develop/Runtime/DataManagment/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Assets._Project.Develop.Runtime.Gameplay.Features.Wallet;
+
+namespace _Project.Develop.Runtime.DataManagment
+{
+    public class PlayerData
+    {
+        public Dictionary<CurrencyTypes, int> WalletData;   
+    }
+}
