fdb18f20a270b03544ee0c8725558a56ec4760bd
Vadim <snuux@yandex.ru>
2026-01-31 13:22:49 +0300

    Moved folders

---
+++ Assets/_Project/Develop/Runtime/Meta/ChangeSceneByLevelTypeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using _Project.Develop.Runtime.Infrastructure.ConfigsManagment;
+using _Project.Develop.Runtime.Infrastructure.CoroutineManagment;
+using _Project.Develop.Runtime.Utilities.SceneManagment;
+using Assets._Project.Develop.Runtime.Configs;
+using UnityEngine;
+
+namespace _Project.Develop.Runtime.Infrastructure.Meta.Infrastructure
+{
+    class ChangeSceneByLevelTypeService
+    {
+        private ConfigsProviderService _configsProviderService;
+        private SceneSwitcherService _sceneSwitcherService;
+        private ICoroutinesPerformer _coroutinesPerformer;
+
+        public ChangeSceneByLevelTypeService(ConfigsProviderService configsProviderService, SceneSwitcherService sceneSwitcherService, ICoroutinesPerformer coroutinesPerformer)
+        {
+            _configsProviderService = configsProviderService;
+            _sceneSwitcherService = sceneSwitcherService;
+            _coroutinesPerformer = coroutinesPerformer;
+        }
+
+        public void Update(Action<float> updateFunc, float deltaTime)
+        {
+            
+        }
+
+        public void ChangeSceneBy(LevelTypes type)
+        {
+            LevelsConfig levelsConfig = _configsProviderService.GetConfig<LevelsConfig>();
+            LevelConfig levelConfig = levelsConfig.GetLevelConfigBy(type);
+
+            _coroutinesPerformer.StartPerform(_sceneSwitcherService.ProcessSwitchTo(
+                Scenes.Gameplay, new GameplayInputArgs(
+                    levelConfig.Length,
+                    levelConfig.Symbols
+                )));
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuContextRegistrations.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Develop.Runtime.Infrastructure.ConfigsManagment;
+using _Project.Develop.Runtime.Infrastructure.CoroutineManagment;
+using _Project.Develop.Runtime.Infrastructure.DI;
+using _Project.Develop.Runtime.Infrastructure.Meta.Infrastructure;
+using _Project.Develop.Runtime.Utilities.SceneManagment;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Infrastructure.Meta.Infrastructure
+{
+    public class MainMenuContextRegistrations
+    {
+        public static void Process(DIContainer container)
+        {
+            Debug.Log("Process of services registration in menu scene");
+
+            container.RegisterAsSingle(CreateChangeSceneByLevelTypeService);
+        }
+
+        private static ChangeSceneByLevelTypeService CreateChangeSceneByLevelTypeService(DIContainer c)
+        {
+            ConfigsProviderService configsProviderService = c.Resolve<ConfigsProviderService>();
+            SceneSwitcherService sceneSwitcherService = c.Resolve<SceneSwitcherService>();
+            ICoroutinesPerformer coroutinesPerformer = c.Resolve<ICoroutinesPerformer>();
+
+            return new ChangeSceneByLevelTypeService(configsProviderService, sceneSwitcherService, coroutinesPerformer);
+        }
+    }
+}
